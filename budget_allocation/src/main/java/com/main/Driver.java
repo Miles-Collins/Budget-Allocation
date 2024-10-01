0396071c88b514876a455e585b6debb3397df613
Miles Collins <102513373+Miles-Collins@users.noreply.github.com>
2024-09-30 22:30:31 -0700

    budget allocated

--- budget_allocation/src/main/java/com/main/Driver.java
+++ budget_allocation/src/main/java/com/main/Driver.java
@@ -8,9 +8,9 @@ public class Driver {
       System.out.println("How much can you spend?");
       Scanner scan = new Scanner(System.in);
       Double total = scan.nextDouble();
-      Double sum;
+      Double sum = 0.0;
       int i = 0;
-      ArrayList<Double> proportion; new ArrayList<>();
+      ArrayList<Double> proportion = new ArrayList<>();
       System.out.println("Enter the proportions of various expenses.");
       System.out.println("The system stops if the cumulative proportion exceeds 100%");
 
@@ -20,7 +20,19 @@ public class Driver {
           double value = scan.nextDouble();
           proportion.add(value);
           sum += proportion.get(i);
+          i++;
       } while(sum <= 100);
-      scan.close();   
+      scan.close();
+      if(sum > 100) {
+        double cumulativeSum = 0.0;
+        for(int j = 0; j < proportion.size(); j++) {
+          cumulativeSum += proportion.get(j);
+        }
+        proportion.set(proportion.size() - 1, 100.0 - cumulativeSum);
+      }
+      for(double value : proportion) {
+        double expense = value * total / 100.0;
+        System.out.println("Your " + value + "% equals $" + expense);
+      }   
   }
 }
